7b635fad93bc088c77683320a9d4b94a7ca5050f
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-08 17:54:43 +0200

    Article model relations updated

--- routes/web.php
+++ routes/web.php
@@ -2,18 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-
 Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
     Route::get('/', 'IndexController')->name('admin.index');
     Route::group(['namespace' => 'Article', 'prefix' => 'articles'], function () {
