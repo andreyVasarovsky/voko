24ff9e9f3cbab8248ae3aceacf1939edd2e4f11d
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-08 10:04:28 +0200

    Article show single page updated

--- resources/views/admin/article/show.blade.php
+++ resources/views/admin/article/show.blade.php
@@ -9,17 +9,7 @@
                 <div class="container-fluid">
                     <div class="row mb-2">
                         <div class="col-sm-6 align-items-center">
-                            <h1 class="m-0 d-inline">Просмотр статьи</h1>
-                            <a href="{{ route('admin.article.edit', $article->id) }}" class="link-icon">
-                                <i class="fas fa-edit"></i>
-                            </a>
-                            <form action="{{ route('admin.article.destroy', $article->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="border-0 bg-transparent">
-                                    <i class="fas fa-trash-alt link-icon text-danger" role="button"></i>
-                                </button>
-                            </form>
+                            <h1 class="m-0 d-inline">Статья: {{ $article->title }}</h1>
                         </div>
                         @if(session('error'))
                             <div class="col-12 alert alert-danger">{{ session('error') }}</div>
@@ -30,29 +20,23 @@
             <section class="content">
                 <div class="container-fluid">
                     <div class="row">
-                        <div class="col-12">
-                            <table class="table close-borders">
-                                <thead>
-                                <tr>
-                                    <th scope="col">#</th>
-                                    <th scope="col">Название</th>
-                                    <th scope="col">Описание</th>
-                                    <th scope="col">Контент</th>
-                                    <th scope="col">Превью</th>
-                                </tr>
-                                </thead>
-                                <tbody>
-                                <tr>
-                                    <th scope="row">{{ $article->id }}</th>
-                                    <td>{{ $article->title }}</td>
-                                    <td>{{ $article->desc }}</td>
-                                    <td>{{ $article->content }}</td>
-                                    <td>{{ $article->preview }}</td>
-                                </tr>
-                                </tbody>
-                            </table>
-
-                            <a href="{{ route('admin.article.index') }}" type="button" class="btn btn-danger">Вернутся</a>
+                        <div class="col-4">
+                            <img class="w-100" src="{{ asset($article->preview) }}" alt="Картинка">
+                            <div class="desc text-center mt-2">
+                                {{ $article->desc }}
+                            </div>
+                            <div class="actions text-center mt-2">
+                                <a href="{{ route('admin.article.index') }}" type="button" class="btn btn-primary">Редактировать</a>
+                                <form action="{{ route('admin.article.destroy', $article->id) }}" method="POST" class="d-inline">
+                                    @csrf
+                                    @method('DELETE')
+                                    <a href="{{ route('admin.article.index') }}" type="button" class="btn btn-danger">Удалить</a>
+                                </form>
+                                <a href="{{ route('admin.article.index') }}" type="button" class="btn btn-secondary">Вернутся</a>
+                            </div>
+                        </div>
+                        <div class="col-8 text-center">
+                            {!! $article->content !!}
                         </div>
                     </div>
                 </div>
