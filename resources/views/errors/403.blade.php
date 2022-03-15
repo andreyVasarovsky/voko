97ec45d46d857ba98d1b02174756a1942ace895e
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-15 17:33:46 +0200

    Laravel permissions backend appended

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-12 text-center mb-0">
+                <h1 style="font-size: 30vw">403</h1>
+            </div>
+        </div>
+    </div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'], function () {
+Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin', 'middleware' => ['can:admin_panel_access']], function () {
     Route::get('/', 'IndexController')->name('admin.index');
-    Route::group(['namespace' => 'Article', 'prefix' => 'articles'], function () {
+    Route::group(['namespace' => 'Article', 'prefix' => 'articles', 'middleware' => ['can:article_access']], function () {
         Route::get('/', 'IndexController')->name('admin.article.index');
         Route::get('/create', 'CreateController')->name('admin.article.create');
         Route::get('/{article}', 'ShowController')->name('admin.article.show');
@@ -14,7 +14,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::patch('/{article}', 'UpdateController')->name('admin.article.update');
         Route::delete('/{article}', 'DestroyController')->name('admin.article.destroy');
     });
-    Route::group(['namespace' => 'Tag', 'prefix' => 'tags'], function () {
+    Route::group(['namespace' => 'Tag', 'prefix' => 'tags', 'middleware' => ['can:tag_access']], function () {
         Route::get('/', 'IndexController')->name('admin.tag.index');
         Route::get('/create', 'CreateController')->name('admin.tag.create');
         Route::get('/{tag}', 'ShowController')->name('admin.tag.show');
@@ -23,7 +23,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::patch('/{tag}', 'UpdateController')->name('admin.tag.update');
         Route::delete('/{tag}', 'DestroyController')->name('admin.tag.destroy');
     });
-    Route::group(['namespace' => 'Category', 'prefix' => 'categories'], function () {
+    Route::group(['namespace' => 'Category', 'prefix' => 'categories', 'middleware' => ['can:category_access']], function () {
         Route::get('/', 'IndexController')->name('admin.category.index');
         Route::get('/create', 'CreateController')->name('admin.category.create');
         Route::get('/{category}', 'ShowController')->name('admin.category.show');
@@ -32,7 +32,7 @@ Route::group(['namespace' => 'App\Http\Controllers\Admin', 'prefix' => 'admin'],
         Route::patch('/{category}', 'UpdateController')->name('admin.category.update');
         Route::delete('/{category}', 'DestroyController')->name('admin.category.destroy');
     });
-    Route::group(['namespace' => 'User', 'prefix' => 'users'], function () {
+    Route::group(['namespace' => 'User', 'prefix' => 'users', 'middleware' => ['can:user_access']], function () {
         Route::get('/', 'IndexController')->name('admin.user.index');
         Route::get('/create', 'CreateController')->name('admin.user.create');
         Route::get('/{user}', 'ShowController')->name('admin.user.show');
