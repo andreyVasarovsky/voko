ed8616a5ff88556086fadef400a6a213e2b59958
Andrej Vasarovskij <andrej.vasarovskij@gmail.com>
2022-03-23 18:56:28 +0200

    Subscribe button added into user view

--- resources/views/public/user/show.blade.php
+++ resources/views/public/user/show.blade.php
@@ -4,7 +4,22 @@
     <div class="container">
         <div class="row">
             <div class="col-12">
-                <h1>Статьи пользователя: {{ $author->name }}</h1>
+                <h1 class="d-inline">Статьи пользователя: {{ $author->name }}</h1>
+                @auth
+                    @if($author->id !== Auth::user()->id)
+                        <div class="d-inline">
+                            @if(Auth::user()->subscriptions->contains('subscribe_user_id', $author->id))
+                                <a href="" style="font-size: 32px;" class="text-secondary">
+                                    <i class="fas fa-minus-square"></i>
+                                </a>
+                            @else
+                                <a href="" style="font-size: 32px;" class="text-danger">
+                                    <i class="fas fa-plus-square"></i>
+                                </a>
+                            @endif
+                        </div>
+                    @endif
+                @endauth
             </div>
         </div>
         @if($articles->count() > 0)
